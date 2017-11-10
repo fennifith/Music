b5c834c3ae8b285a1d47635277bc4c4b9b634d1d
TheAndroidMaster <18jafenn90@gmail.com>
2017-11-10 13:46:51 -0500

    added practice elements/js

--- js/data.js
+++ js/data.js
@@ -22,10 +22,42 @@ function loadPractice(element) {
       if (dataRequest.readyState === 4) {
         if (dataRequest.status === 200 || dataRequest.status == 0) {
           var obj = JSON.parse(dataRequest.responseText);
-					//TODO: make request to
+					for (var i = 0; obj[i]; i++) {
+							loadYouTubePractice(obj[i], element);
+					}
         }
       }
     }
     dataRequest.open("GET", "https://theandroidmaster.github.io/Music/practice/data.json", true);
     dataRequest.send(null);
+}
+
+function loadYouTubePractice(id, element) {
+	var dataRequest = createCORSRequest("GET", "https://noembed.com/embed?url=" + getYouTubeUrl(id) + "&format=json");
+    dataRequest.onreadystatechange = function () {
+      if (dataRequest.readyState === 4) {
+        if (dataRequest.status === 200 || dataRequest.status == 0) {
+          var obj = JSON.parse(dataRequest.responseText);
+					element.innerHTML += "<div class=\"practice\" onclick=\"location.href = \'" + obj.src + "\';\"><img src=\"" + obj.thumbnail_url +"\"><div class=\"info\"><p>" + obj.title + "</p></div></div>";
+        }
+      }
+    }
+    dataRequest.send(null);
+}
+
+function getYouTubeUrl(id) {
+	return "https://youtu.be/" + id;
+}
+
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    xhr = null;
+  }
+  return xhr;
 }
